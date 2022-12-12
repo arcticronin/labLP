bf8523a4593573a3e3373e0a7d42c1d75ec82709
Luca Manzi <ronin.git@gmail.com>
2022-12-12 14:21:39 +0100

    Add files via upload

---
+++ labLP-main/labLP-main/3/y.tab.c
@@ -0,0 +1,364 @@
+#ifndef lint
+static char yysccsid[] = "@(#)yaccpar	1.8 (Berkeley) 01/20/90";
+#endif
+#define YYBYACC 1
+#line 2 "group2.y"
+  import java.io.*;
+#line 8 "y.tab.c"
+#define YYERRCODE 256
+short yylhs[] = {                                        -1,
+    0,    0,    0,    0,    1,    1,    2,    2,    3,    3,
+};
+short yylen[] = {                                         2,
+    0,    2,    2,    2,    1,    2,    1,    2,    1,    2,
+};
+short yydefred[] = {                                      1,
+    0,    5,    7,    9,    0,    0,    4,    6,    8,   10,
+};
+short yydgoto[] = {                                       1,
+    5,    6,    7,
+};
+short yysindex[] = {                                      0,
+  -39,    0,    0,    0,  -45,  -35,    0,    0,    0,    0,
+};
+short yyrindex[] = {                                      0,
+    0,    0,    0,    0,    2,    1,    0,    0,    0,    0,
+};
+short yygindex[] = {                                      0,
+    0,    0,    0,
+};
+#define YYTABLESIZE 46
+short yytable[] = {                                       8,
+    3,    2,    3,    4,    0,    2,    9,   10,    0,    0,
+    0,    0,    0,    0,    0,    0,    0,    0,    0,    0,
+    0,    0,    0,    0,    0,    0,    0,    0,    0,    0,
+    0,    0,    0,    0,    0,    0,    0,    0,    0,    0,
+    0,    0,    0,    2,    2,    3,
+};
+short yycheck[] = {                                      45,
+    0,    0,   42,   43,   -1,   45,   42,   43,   -1,   -1,
+   -1,   -1,   -1,   -1,   -1,   -1,   -1,   -1,   -1,   -1,
+   -1,   -1,   -1,   -1,   -1,   -1,   -1,   -1,   -1,   -1,
+   -1,   -1,   -1,   -1,   -1,   -1,   -1,   -1,   -1,   -1,
+   -1,   -1,   -1,   42,   43,   45,
+};
+#define YYFINAL 1
+#ifndef YYDEBUG
+#define YYDEBUG 0
+#endif
+#define YYMAXTOKEN 45
+#if YYDEBUG
+char *yyname[] = {
+"end-of-file",0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+0,0,0,0,0,0,0,0,"'*'","'+'",0,"'-'",
+};
+char *yyrule[] = {
+"$accept : input",
+"input :",
+"input : input negS",
+"input : input timesS",
+"input : input plusS",
+"negS : '-'",
+"negS : negS '-'",
+"timesS : '*'",
+"timesS : timesS '*'",
+"plusS : '+'",
+"plusS : timesS '+'",
+};
+#endif
+#ifndef YYSTYPE
+typedef int YYSTYPE;
+#endif
+#define yyclearin (yychar=(-1))
+#define yyerrok (yyerrflag=0)
+#ifdef YYSTACKSIZE
+#ifndef YYMAXDEPTH
+#define YYMAXDEPTH YYSTACKSIZE
+#endif
+#else
+#ifdef YYMAXDEPTH
+#define YYSTACKSIZE YYMAXDEPTH
+#else
+#define YYSTACKSIZE 500
+#define YYMAXDEPTH 500
+#endif
+#endif
+int yydebug;
+int yynerrs;
+int yyerrflag;
+int yychar;
+short *yyssp;
+YYSTYPE *yyvsp;
+YYSTYPE yyval;
+YYSTYPE yylval;
+short yyss[YYSTACKSIZE];
+YYSTYPE yyvs[YYSTACKSIZE];
+#define yystacksize YYSTACKSIZE
+#line 26 "group2.y"
+
+void yyerror(String s)
+{
+ System.out.println("err:"+s);
+ System.out.println("   :"+yylval.sval);
+}
+
+static Yylex lexer;
+int yylex()
+{
+ try {
+  return lexer.yylex();
+ }
+ catch (IOException e) {
+  System.err.println("IO error :"+e);
+  return -1;
+ }
+}
+
+public static void main(String args[])
+{
+ System.out.println("[Quit with CTRL-D]");
+ Parser par = new Parser();
+ lexer = new Yylex(new InputStreamReader(System.in), par);
+ par.yyparse();
+}
+#line 125 "y.tab.c"
+#define YYABORT goto yyabort
+#define YYACCEPT goto yyaccept
+#define YYERROR goto yyerrlab
+int
+yyparse()
+{
+    register int yym, yyn, yystate;
+#if YYDEBUG
+    register char *yys;
+    extern char *getenv();
+
+    if (yys = getenv("YYDEBUG"))
+    {
+        yyn = *yys;
+        if (yyn >= '0' && yyn <= '9')
+            yydebug = yyn - '0';
+    }
+#endif
+
+    yynerrs = 0;
+    yyerrflag = 0;
+    yychar = (-1);
+
+    yyssp = yyss;
+    yyvsp = yyvs;
+    *yyssp = yystate = 0;
+
+yyloop:
+    if (yyn = yydefred[yystate]) goto yyreduce;
+    if (yychar < 0)
+    {
+        if ((yychar = yylex()) < 0) yychar = 0;
+#if YYDEBUG
+        if (yydebug)
+        {
+            yys = 0;
+            if (yychar <= YYMAXTOKEN) yys = yyname[yychar];
+            if (!yys) yys = "illegal-symbol";
+            printf("yydebug: state %d, reading %d (%s)\n", yystate,
+                    yychar, yys);
+        }
+#endif
+    }
+    if ((yyn = yysindex[yystate]) && (yyn += yychar) >= 0 &&
+            yyn <= YYTABLESIZE && yycheck[yyn] == yychar)
+    {
+#if YYDEBUG
+        if (yydebug)
+            printf("yydebug: state %d, shifting to state %d (%s)\n",
+                    yystate, yytable[yyn],yyrule[yyn]);
+#endif
+        if (yyssp >= yyss + yystacksize - 1)
+        {
+            goto yyoverflow;
+        }
+        *++yyssp = yystate = yytable[yyn];
+        *++yyvsp = yylval;
+        yychar = (-1);
+        if (yyerrflag > 0)  --yyerrflag;
+        goto yyloop;
+    }
+    if ((yyn = yyrindex[yystate]) && (yyn += yychar) >= 0 &&
+            yyn <= YYTABLESIZE && yycheck[yyn] == yychar)
+    {
+        yyn = yytable[yyn];
+        goto yyreduce;
+    }
+    if (yyerrflag) goto yyinrecovery;
+#ifdef lint
+    goto yynewerror;
+#endif
+yynewerror:
+    yyerror("syntax error");
+#ifdef lint
+    goto yyerrlab;
+#endif
+yyerrlab:
+    ++yynerrs;
+yyinrecovery:
+    if (yyerrflag < 3)
+    {
+        yyerrflag = 3;
+        for (;;)
+        {
+            if ((yyn = yysindex[*yyssp]) && (yyn += YYERRCODE) >= 0 &&
+                    yyn <= YYTABLESIZE && yycheck[yyn] == YYERRCODE)
+            {
+#if YYDEBUG
+                if (yydebug)
+                    printf("yydebug: state %d, error recovery shifting\
+ to state %d\n", *yyssp, yytable[yyn]);
+#endif
+                if (yyssp >= yyss + yystacksize - 1)
+                {
+                    goto yyoverflow;
+                }
+                *++yyssp = yystate = yytable[yyn];
+                *++yyvsp = yylval;
+                goto yyloop;
+            }
+            else
+            {
+#if YYDEBUG
+                if (yydebug)
+                    printf("yydebug: error recovery discarding state %d\n",
+                            *yyssp);
+#endif
+                if (yyssp <= yyss) goto yyabort;
+                --yyssp;
+                --yyvsp;
+            }
+        }
+    }
+    else
+    {
+        if (yychar == 0) goto yyabort;
+#if YYDEBUG
+        if (yydebug)
+        {
+            yys = 0;
+            if (yychar <= YYMAXTOKEN) yys = yyname[yychar];
+            if (!yys) yys = "illegal-symbol";
+            printf("yydebug: state %d, error recovery discards token %d (%s)\n",
+                    yystate, yychar, yys);
+        }
+#endif
+        yychar = (-1);
+        goto yyloop;
+    }
+yyreduce:
+#if YYDEBUG
+    if (yydebug)
+        printf("yydebug: state %d, reducing by rule %d (%s)\n",
+                yystate, yyn, yyrule[yyn]);
+#endif
+    yym = yylen[yyn];
+    yyval = yyvsp[1-yym];
+    switch (yyn)
+    {
+case 1:
+#line 7 "group2.y"
+{ yyval = new ParserVal("");
+                    System.out.println(": \""+yyval.sval+"\""); }
+break;
+case 2:
+#line 9 "group2.y"
+{ yyval = new ParserVal(yyvsp[-1].sval+yyvsp[0].sval);
+                   System.out.println(": "+yyval.sval); }
+break;
+case 3:
+#line 11 "group2.y"
+{ yyval = new ParserVal(yyvsp[-1].sval+yyvsp[0].sval);
+                   System.out.println(": "+yyval.sval); }
+break;
+case 4:
+#line 13 "group2.y"
+{ yyval = new ParserVal(yyvsp[-1].sval+yyvsp[0].sval);
+                   System.out.println(": "+yyval.sval); }
+break;
+case 5:
+#line 16 "group2.y"
+{ yyval = yyvsp[0]; }
+break;
+case 6:
+#line 17 "group2.y"
+{ yyval = new ParserVal(yyvsp[-1].sval + '-'); }
+break;
+case 7:
+#line 19 "group2.y"
+{yyval = yyvsp[0]; }
+break;
+case 8:
+#line 20 "group2.y"
+{ yyval = new ParserVal(yyvsp[-1].sval + '*'); }
+break;
+case 9:
+#line 22 "group2.y"
+{yyval = yyvsp[0]; }
+break;
+case 10:
+#line 23 "group2.y"
+{ yyval = new ParserVal(yyvsp[-1].sval + '+'); }
+break;
+#line 309 "y.tab.c"
+    }
+    yyssp -= yym;
+    yystate = *yyssp;
+    yyvsp -= yym;
+    yym = yylhs[yyn];
+    if (yystate == 0 && yym == 0)
+    {
+#if YYDEBUG
+        if (yydebug)
+            printf("yydebug: after reduction, shifting from state 0 to\
+ state %d\n", YYFINAL);
+#endif
+        yystate = YYFINAL;
+        *++yyssp = YYFINAL;
+        *++yyvsp = yyval;
+        if (yychar < 0)
+        {
+            if ((yychar = yylex()) < 0) yychar = 0;
+#if YYDEBUG
+            if (yydebug)
+            {
+                yys = 0;
+                if (yychar <= YYMAXTOKEN) yys = yyname[yychar];
+                if (!yys) yys = "illegal-symbol";
+                printf("yydebug: state %d, reading %d (%s)\n",
+                        YYFINAL, yychar, yys);
+            }
+#endif
+        }
+        if (yychar == 0) goto yyaccept;
+        goto yyloop;
+    }
+    if ((yyn = yygindex[yym]) && (yyn += yystate) >= 0 &&
+            yyn <= YYTABLESIZE && yycheck[yyn] == yystate)
+        yystate = yytable[yyn];
+    else
+        yystate = yydgoto[yym];
+#if YYDEBUG
+    if (yydebug)
+        printf("yydebug: after reduction, shifting from state %d \
+to state %d\n", *yyssp, yystate);
+#endif
+    if (yyssp >= yyss + yystacksize - 1)
+    {
+        goto yyoverflow;
+    }
+    *++yyssp = yystate;
+    *++yyvsp = yyval;
+    goto yyloop;
+yyoverflow:
+    yyerror("yacc stack overflow");
+yyabort:
+    return (1);
+yyaccept:
+    return (0);
+}
